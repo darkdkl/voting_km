51a633d68dfc82924a0e164253dd0bc2c62e9cd7
Dark_Dmake <dark.dmake@gmail.com>
2020-09-19 18:29:16 +0300

    add button "export xls",add middleware (current user)

--- voting/admin.py
+++ voting/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .middleware.current_user import get_current_user
 from .models import Voting,Persona
 # Register your models here.
-admin.site.register(Voting)
+
 admin.site.register(Persona)
+
+@admin.register(Voting)
+class VotingAdmin(admin.ModelAdmin):
+
+    def export_to_xls(self, obj):
+        print(obj.id)
+        print(get_current_user())
+        return mark_safe('<button>XLS</button>')
+
+    list_display = ('name', 'date_start', 'date_finish', 'early_count',
+                    'completed',"export_to_xls")
--- votings_km/settings.py
+++ votings_km/settings.py
@@ -48,7 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'voting.middleware.check_time_middleware.CheckTimeMiddleware'
+    'voting.middleware.check_time_middleware.CheckTimeMiddleware',
+    'voting.middleware.current_user.CurrentUserMiddleware'
 ]
 
 ROOT_URLCONF = 'votings_km.urls'
